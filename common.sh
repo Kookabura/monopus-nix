1145fdb2ebf6499ec540b8b46797c8fdf4fe65ee
Anatoly Seregin <aseregin@tolkit.top>
2021-01-19 23:00:11 +0300

    first commit

---
+++ common.sh
@@ -0,0 +1,57 @@
+# Find 
+which ed 2>&- >/dev/null && ED=ed 
+which ex 2>&- >/dev/null && ED=ex 
+
+BASE=/opt/monopus
+VAR=/var/monopus
+CFG_FN=/etc/monopus.cfg # Config file to store information in
+CHECKS_FN=$VAR/checks.txt # Where we store checks to perform with their times
+PORTAL_URL=http://portal.monopus.ru/test-api.html
+
+PRINTF=/usr/bin/printf # Better printf for old linuxen, that supports \uNNNN
+test -f $PRINTF || PRINTF=printf # Use builtin
+
+# Synopsis: set_param <config_file> <param> <value>
+set_param()
+{
+  # For empty file or file without such parameter, otherwise d fails
+  #echo "$2=" >> $1
+  $ED -s $1 <<EOF
+g/^ *"$2": "/d
+i
+    "$2": "$3",
+.
+w
+q
+EOF
+}
+
+# Gets and outputs to stdout the value from JSON (passed as <var>) for the given
+# key.
+# get_opt <var> <name>
+get_opt()
+{
+  echo "$1" | awk -F"\t" '$1=="[\"'$2'\"]"{print $2}'
+}
+
+# logs to logger its arguments, if there are no arguments, log stdin
+log()
+{  
+  [ "$verbose" ] || return
+  # log_console may be set to 's' to output to the stderr as well  
+  if [ $# = 0 ]; then cat; else echo "$*"; fi |
+#  sed "s/^/`date`: /g" | 
+  logger -${log_console}t monopus
+}
+
+check_curl()
+{
+  if which curl >/dev/null; then
+    RETR="curl -s"
+  elif which wget >/dev/null; then
+    RETR="wget -qO-"
+  else
+    log "curl or wget must be installed to run the script"
+    exit 1
+  fi
+}
---
+++ install.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+
+log_console=s
+verbose=1
+wait_time=360 # Default time to wait before running a newly added check
+. ./common.sh
+
+usage ()
+{
+  cat <<EOF >&2
+ `basename $0`: Install monopus monitoring software
+sh ./`basename $0` [-hd] -k <api_key>
+ 	-h 			- Print this message
+ 	-d 			- Deinstall monopus monitor
+ 	-k<api_key>		- API_KEY (get in your profile)
+Examples:
+sh ./`basename $0` -kPtXwn4Stv3UdbmvEyEKHfvKEVdGyWN2Lh0iF4b8Ae2Oppj4YYO - Initialise subscription
+EOF
+  exit 0
+}
+
+do_deinstall()
+{
+  log "Deinstalling programs"
+
+  # Send stop command
+
+  # id=$(cat $CFG_FN | get_param id) # hostname
+  # CFG_API_KEY=$(cat $CFG_FN | get_param api_key)
+  # check_curl
+  # res=$($RETR "$PORTAL_URL?api_key=$CFG_API_KEY&action=delete&host_id=$id"|./json.sh -l)
+  # err=$(get_opt "$res" error)
+  # if [ "$err" ]; then
+  #   msg=$($PRINTF "$err")
+  #   log "Error deleting server: $msg"
+  #   exit 1
+  # fi
+  # result=$(get_opt "$res" result)
+  # log "Unregistration result: $($PRINTF "$result")"
+  # echo
+
+  # if [ -f /etc/redhat-release ]; then
+  #   case $(cut -f1 -d' ' < /etc/redhat-release) in
+  #     CentOS)
+  # 	service monopus stop
+  # 	chkconfig --del monopus;;
+  #   esac
+  #   rm /etc/init.d/monopus
+  # elif [ -f /etc/debian_version ]; then
+  #   service monopus stop
+  #   rm /etc/init.d/monopus
+  #   update-rc.d monopus remove
+  # else
+  #   : # Some other OS, not supported yet
+  # fi
+  systemctl disable monopus
+  
+  rm -rf /etc/monopus.cfg $BASE $VAR
+  
+  log "Program deinstalled"
+  exit 0
+}
+
+do_install()
+{
+  log "Installing programs"
+  # Installing to /opt/monopus, also need /var/monopus
+  for i in $BASE/check_scripts $VAR; do test -d $i || mkdir -p $i; done
+  cp common.sh json.sh monopus $BASE # send_nrdp.sh
+  cp -r check_scripts/* $BASE/check_scripts
+  cp monopus.cfg $CFG_FN
+  
+  # if [ -f /etc/redhat-release ]; then
+  #   case $(cut -f1 -d' ' < /etc/redhat-release) in
+  #     CentOS)
+  # 	cp init.redhat /etc/init.d/monopus
+  # 	chkconfig --levels 234 monopus on
+  # 	service monopus start
+  # 	;;
+  #   esac
+  # elif [ -f /etc/debian_version ]; then # At least Ubuntu
+  #   cp init.debian /etc/init.d/monopus
+  #   update-rc.d monopus defaults
+  #   service monopus start
+  # else
+  #   : # Some other OS, not supported yet
+  # fi
+
+  cp monopus.service /etc/systemd/system/
+  systemctl enable monopus
+  systemctl start monopus
+  
+  log "Programs installed"
+}
+
+# major: ${1%.*} minor: ${1#*.}
+# returns true if version in $1 is lower (<) than in $2
+ver_lt()
+{
+  [ ${1%.*} -lt ${2%.*} ] || [ ${1%.*} -eq ${2%.*} -a ${1#*.} -lt ${2#*.} ]
+}
+
+while getopts "?di:k:" opt; do
+  case $opt in
+    k) CFG_API_KEY=$OPTARG;;
+    d) do_deinstall;;
+    ?) usage;;
+  esac
+done
+shift $((OPTIND-1))
+
+[ $(id -u) -ne 0 ] && { log "Need root privileges to install"; exit 1; }
+
+check_curl
+
+hostname=$(hostname)
+if [ -f $CFG_FN ]; then
+  log "Monopus is already installed, checking if we need to update files"
+  our_ver=$(cat $CFG_FN | get_param version)
+  new_ver=$(cat version.txt)
+  if ver_lt $new_ver ${our_ver:-0.00}; then
+    log "You already have the latest version."
+    exit 1
+  fi
+  # Stop service for supported distribs
+  # [ -f /etc/redhat-release -o -f /etc/debian_version ] && service monopus stop
+  systemctl stop monopus
+else
+  do_init=1
+
+  [ "$CFG_API_KEY" ] || { log "API key is mandatory"; usage; exit 1; }
+  #create_object
+fi
+
+do_install
+
+
+[ "$CFG_API_KEY" ] && set_param $CFG_FN api_key $CFG_API_KEY
+
