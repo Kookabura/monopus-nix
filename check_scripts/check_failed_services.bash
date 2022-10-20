e05f05c787bd34a006b12def844bd0f2a22db451
Alexander Shevchuk <79712453+Anrikko@users.noreply.github.com>
2022-10-20 11:24:48 +0300

    Fixed script

--- check_scripts/check_failed_services.bash
+++ check_scripts/check_failed_services.bash
@@ -2,18 +2,18 @@
 
 if [ -f /bin/systemctl ]; then
 	failed=$(/bin/systemctl --failed --no-legend)
-	failed=${failed/ */}		# Strip everything after first space
-	failed=${failed/.service/}	# Strip .service suffix
+	failed=${failed/ */}  # Strip everything after first space
+	failed=${failed/.service/} # Strip .service suffix
 
 	if [ "$failed" != "" ]; then
 		#echo "Failed units: ${failed[@]} || ${#failed[@]}"
 		echo "check_failed_services.critical::services==$failed | services=${#failed[@]};;;"
 		exit 2
 	else
-		echo "check_failed_services.ok::services==services==$failed | services=${#failed[@]};;;"
+		echo "check_failed_services.ok | services=0;;;"
 		exit 0
 	fi
-else
-	echo "check_failed_services.unknown::services==services==$failed | services=${#failed[@]};;;"
-	exit 3
+	else
+		echo "check_failed_services.unknown | services=0;;;"
+		exit 3
 fi
