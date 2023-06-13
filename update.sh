9a749b1937e35409cb62c94489c823c0b730a9a9
SerG0zzz <SeroEgor@Gmail.com>
2023-06-13 23:03:28 +0300

    update monopus script ver. 1

---
+++ update.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Script update monopus
+INST=/opt/mondistr      # Dir from install update
+CFG_FN=/etc/monopus.cfg # Config file to store information in
+UpdateLink=""           # From Update
+Version="1.0"           # Version script
+
+# Print help for Monopus
+print_help() {
+  echo "Monopus update script. Usage:"
+  echo "update [-h] [-v]"
+  echo "  -h     - Print this message"
+  echo "  -v     - Print version monopus"
+  echo "  <link> - Update monopus, default from: https://github.com/Kookabura/monopus-nix/archive/refs/heads/main.zip"
+  exit 0
+}
+
+# Print version script update Monopus
+print_version() {
+  echo "Update monopus script version $Version"
+  exit 0
+}
+
+# Check update link Monopus
+check_update() {
+  if [ -z "$UpdateLink" ]; then
+    UpdateLink="https://github.com/Kookabura/monopus-nix/archive/refs/heads/main.zip"
+  fi
+  if [ -n "$1" ]; then
+    UpdateLink=$1
+  fi
+}
+# Check exist Monopus
+check_monopus() {
+  if [ -s "$CFG_FN" ];then
+    echo "Check existing Monopus... success"
+  else
+    echo "Update fail. Monopus service do not exist!!! Please install him:"
+    echo "${INST}/install.sh -k<api_key> or sudo ${INST}/install.sh -k<api_key>"
+    exit 1
+  fi
+}
+
+while getopts "hv?" opt; do
+  case $opt in
+    v) print_version;;
+    h|?) print_help;;
+    *) exit 0;;
+  esac
+done
+shift $((OPTIND-1))
+check_update "$1"
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e "\n\nNeed root privileges to update monopus!!! Exit";
+  exit 1;
+fi
+echo "Starting update monopus..."
+echo "Create Install directory."
+mkdir -p $INST
+if wget -c -q -O ${INST}/update.zip "$UpdateLink";then
+  echo "Download monopus from link: success"
+else
+  echo "Download monopus from link: fail!  exit..."
+  exit 1
+fi
+echo "Unpack files"
+unzip -tq ${INST}/update.zip && unzip -oq -d $INST ${INST}/update.zip || exit 1
+updatedir=$(ls -xd ${INST}/monopus-*/)
+mv -f "$updatedir"* $INST
+rm -rf  ${INST}/update.zip "$updatedir"
+echo "Adding execute permissions to files"
+if chmod +x -R ${INST};then
+  echo "All right installed successful"
+else
+  echo "Fail set execution rights... exit"
+  exit 1
+fi
+echo "Remove execution permissions from .service, .cfg and .md"
+chmod 644 ${INST}/monopus.service ${INST}/monopus.cfg ${INST}/*.md
+check_monopus
+echo "I start the update through a script call: install.sh -u"
+cd ${INST} || echo "Unable to change directory!"
+if bash ${INST}/install.sh -u; then
+  echo "Update went without error"
+  systemctl daemon-reload
+  echo "Restarting monopus daemon"
+  systemctl restart monopus
+  rm -rf  ${INST}
+  echo "Update saccessful, well done."
+else
+  echo "Install.sh exit with error."
+  systemctl daemon-reload             # in main 1.0.4 exist error, delele this 2 line, after
+  systemctl restart monopus           # correction
+  rm -rf  ${INST}
+fi
