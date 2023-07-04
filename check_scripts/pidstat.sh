bb12227405f0c55d0a9faf7d2f5c6771e84bd2b7
SerG0zzz <SeroEgor@Gmail.com>
2023-07-04 21:32:29 +0300

    fix bug calculate cpu_load (time format AM,PM or not)

--- check_scripts/pidstat.sh
+++ check_scripts/pidstat.sh
@@ -2,4 +2,9 @@
 
 process="$1"
 pidstat=$(/usr/bin/pidstat -u -I -C "$process" 120 1 | grep -v "^$" | grep -v "Average" | tail -n +3)
+if [ "$(echo "$pidstat" | head -n1 | wc -w)" -eq 10 ]; then #remove AM or PM from pidstat
+  pidstat=$(echo "$pidstat" | awk '{print $8}')
+else
+  pidstat=$(echo "$pidstat" | awk '{print $7}')
+fi
 echo "$pidstat" > "/tmp/${process}_2min_stat.txt"
