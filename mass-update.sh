8c495d35a71dc551b4563987f8de90d3f9544dcf
SerG0zzz <SeroEgor@Gmail.com>
2023-08-11 01:55:13 +0300

    initial commit mass-update clients

---
+++ mass-update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#usage:
+#./mass-update.sh <pass-file> <command update> 
+
+copyleft() {
+  echo ""
+  echo "Usage ./mass-update.sh <pass-file> <command update>"
+  echo ""
+  echo "Copyleft TolkIT (team@tolkit.top)"
+  echo "author Sergey Egorushkin (SeroEgor@Gmail.com)"
+}
+
+LOG=mass-update.log                         #create log file
+touch $LOG && echo "" > $LOG
+
+if [ -n "$2" ]; then                        #if <command update> not empty
+  if [ -s "$1" ]; then                      #if <pass-file> exist and greater than zero
+    hosts=$(echo "$( < "$1" wc -l) - 2" | bc -l) # number hosts in <pass-file>
+    if [ "$hosts" -gt 0 ]; then             #if number hosts in <pass-file> greater zero
+      IFS=',' read -ra PASSES <<< "$( < "$1" tail -n "$hosts" | tr -s '\n' ',')" #create array lines with credentials
+      for PASS in "${PASSES[@]}"            # 1 line with credential
+      do
+        pass=$(echo "$PASS" | awk '{print $1}')  #password
+        login=$(echo "$PASS" | awk '{print $2}') #login
+        ip=$(echo "$PASS" | awk '{print $3}')    #ip
+        if sshpass -p "${pass}" ssh -o StrictHostKeyChecking=no "${login}@${ip}" "$2"; then
+           echo "Update on ${login}@${ip} - success" >> $LOG # loggin success
+        else                                                 # loggin fail
+           echo "Update on ${login}@${ip} - fail, exit code $?" >> $LOG
+        fi 
+      done
+    else
+      echo "file with passwords is empty"
+      copyleft
+    fi
+  else
+    echo "File with passwords not exist: $1"
+    copyleft
+  fi
+else
+  echo "Please enter update command as second argument"
+  copyleft
+fi
