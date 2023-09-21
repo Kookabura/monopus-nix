ce87cc57e204ead57005817666560b1da2ff1994
SerG0zzz <SeroEgor@Gmail.com>
2023-09-21 13:36:50 +0300

    try final fix infinity cpu load in pidstat link in check_apache2

--- check_scripts/pidstat.sh
+++ check_scripts/pidstat.sh
@@ -3,8 +3,8 @@
 process="$1"
 pidstat=$(/usr/bin/pidstat -u -I -C "$process" 120 1 | grep -v "^$" | grep -v "Average" | tail -n +3)
 if [ "$(echo "$pidstat" | head -n1 | wc -w)" -eq 10 ]; then #remove AM or PM from pidstat
-  pidstat=$(echo "$pidstat" | awk '{print $8}')
+  pidstat=$(echo "$pidstat" | awk 'length($8) < 6 {print $8}')
 else
-  pidstat=$(echo "$pidstat" | awk '{print $7}')
+  pidstat=$(echo "$pidstat" | awk 'length($8) < 6 {print $7}')
 fi
 echo "$pidstat" > "/tmp/${process}_2min_stat.txt"
