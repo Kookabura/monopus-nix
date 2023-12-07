39c9fd06ac6da988918b54c0080c9956105095ec
SerG0zzz <SeroEgor@Gmail.com>
2023-12-08 00:40:00 +0300

    add case if packet manager in error

--- check_scripts/security-update.sh
+++ check_scripts/security-update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 sec=0
-if [ "$1" = "Centos" ]; then
+if [ "$1" = "Centos" ]; then                # if Centos
     sec=$(yum --security check-update 2>/dev/null | grep "security" | tail -n 1 | awk '{print $1}')
     if [ "$sec" = "No" ]; then
         sec=0
     fi
-elif [ $1 = "Debian" ] || [ "$1" = "Ubuntu" ]; then
+elif [ $1 = "Debian" ] || [ "$1" = "Ubuntu" ]; then # if Debian or Ubuntu
     sec=$(apt list --upgradable 2>/dev/null | grep -c "-security")
 fi
-echo "$sec" > ../tmp/update-security
+if [ -z "$sec" ]; then                      # if return empty - error
+    sec="error"
+fi
+echo "$sec" > ../tmp/update-security        # save result in tmp-file
